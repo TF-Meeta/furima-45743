d1551746dcdbdbe9a0671566ff8c46458ce05b43
TF-Meeta <takahiro.f9135@gmail.com>
2025-07-29 22:11:35 +0900

    商品詳細画面の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
+    @items = Item.includes(:user).order(created_at: :desc)
   end
 
   def new
@@ -16,6 +17,10 @@ class ItemsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
