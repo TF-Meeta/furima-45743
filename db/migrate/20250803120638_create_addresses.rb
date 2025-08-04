b8c727d4e5714ced487206a78bfd559c27967e18
TF-Meeta <takahiro.f9135@gmail.com>
2025-08-04 20:54:36 +0900

    issue解決

--- db/migrate/20250803120638_create_addresses.rb
+++ db/migrate/20250803120638_create_addresses.rb
@@ -1,11 +1,11 @@
 class CreateAddresses < ActiveRecord::Migration[7.1]
   def change
     create_table :addresses do |t|
-      t.string :postal_code
-      t.integer :prefecture_id
+      t.string :postal_code, null: false
+      t.integer :prefecture_id, null: false
       t.string :city, null: false
       t.string :house_number, null: false
-      t.string :building_name, null: false
+      t.string :building_name
       t.string :phone_number, null: false
       t.references :order, null: false, foreign_key: true
 
