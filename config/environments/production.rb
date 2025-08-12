e9ffbc71e0388ef9e8c04d56ed618f48b41805e5
TF-Meeta <takahiro.f9135@gmail.com>
2025-08-12 17:58:08 +0900

    capistrano導入

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,3 +95,6 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
+
+
+server '52.192.156.220', user: 'ec2-user', roles: %w{app db web}
